205cd1d4b566887c2cb3f913e7d1be5e256c11db
nikkijadon <nkkjadon@gmail.com>
2026-02-16 23:16:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+let input = document.querySelector("input");
+
+btn.addEventListener("click", function(){
+    let item = document.createElement("li");
+    item.innerText = input.value;
+
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "delete";
+    delBtn.classList.add("delete");
+    
+    item.appendChild(delBtn)
+    ul.appendChild(item);
+    input.value =" ";
+});
+ul.addEventListener("click", function(event){
+    if(event.target.nodeName == "BUTTON"){
+        let listItem = event.target.parentElement;
+        listItem.remove();
+        console.log("deleted");
+    }
+   
+});
+
+// let delBtns = document.querySelector(".delete");
+// for(delBtn of delBtns){
+//     delBtn.addEventListener("click", function(){
+//         let par = this.parenntElement;
+//         console.log(par);
+//         par.remove();
+//     });
+// }
